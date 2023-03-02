5ed265fe739b9527013d5b2961077e5f859cf267
Drew Bowling <andrewwbowling@gmail.com>
2023-03-02 15:49:44 -0700

    finished cleanup

--- cleanUp.js
+++ cleanUp.js
@@ -8,37 +8,53 @@
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+function askForName() { 
+  return ("hello what is your name"); 
+}
+console.log(askForName());
 
-askForName()
+// What I changed: made the code look cleaner. 
+//Used a return statement to output out the 
+//text "hello what is your name" when the function is called in the console log
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
-      }
+  var sum = first + second + third;
+  return sum;
+}
+console.log(addThreeNums(1, 2, 3));
+console.log(addThreeNums(4, 2, 7));
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
-
+// added a console log to show the function.  
 
 // EX 3:
-func makeFreshPesto(){
-  console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
-  console.log("Pulse basil and pine nuts");
-  console.log("Add garlic and cheeses");
-  console.log("Slowly pour in oil");
-  console.log("Season");    }
+function makeFreshPesto(){
+  var pestoSteps = [
+    "Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper",
+    "Pulse basil and pine nuts",
+    "Add garlic and cheeses",
+    "Slowly pour in oil", "Season"
+  ];
+  for (var i = 0; i < pestoSteps.length; i++) {
+    console.log(pestoSteps[i])
+  }
 
+}
+console.log("these are the steps for making pesto:");
 makeFreshPesto();
+
+// Removed the unnecessary console logs, and turned it into a for statement using a string array
+
 
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
 
-return avg
-  }
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  return avg
+}
+console.log(average(1,2))
+
+// cleaned up the code to be more blocky and easier to read. added a console log to read user inputs through the function
