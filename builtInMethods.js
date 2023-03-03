8cdb15916689a56c70025e6659a2431c8fae163d
Drew Bowling <andrewwbowling@gmail.com>
2023-03-02 17:51:30 -0700

    answered section 3

--- builtInMethods.js
+++ builtInMethods.js
@@ -73,4 +73,5 @@ console.log(phrases.sort());
 var firstName = ["d","r","e","w"]
 
 console.log(firstName.join(``))
-// this is joining the strings together 
+// this is logging the strings together found in the array expressed by the variable firstName
+
